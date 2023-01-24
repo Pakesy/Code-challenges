44b55d749820a77f8acaeb46cc627a577dd81207
Pakesy <conorpakes@gmail.com>
2023-01-24 19:39:07 +1100

    30 days of JS - Day 5 level 2 finished

--- 30-days-of-JS/day-5/level-2/main.js
+++ 30-days-of-JS/day-5/level-2/main.js
@@ -21,3 +21,19 @@ console.log(shoppingCart);
 if (countries.includes("Ethiopia")) {
     console.log("ETHIOPIA");
  } else countries.push("Ethiopia");
+
+ // 5
+
+ if (webTechs.includes("Sass")) {
+    console.log("Sass is a CSS preprocess");
+ } else webTechs.push("Sass");
+ console.log(webTechs);
+
+ // 6
+
+ const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+const backEnd = ['Node','Express', 'MongoDB']
+
+let fullStack = frontEnd.concat(backEnd);
+console.log(fullStack);
+
