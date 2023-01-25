fa9b75d66a8c7721d7ded2db11d400be7dc12c9d
Pakesy <conorpakes@gmail.com>
2023-01-26 10:01:30 +1100

    Finished Day 6

--- 30-days-of-JS/day-6/level2.js
+++ 30-days-of-JS/day-6/level2.js
@@ -1,4 +1,4 @@
-/* Day 6 Level 2 Challenges */ 
+/* Day 6 Level 2 Challenges */
 
 // Task 1
 
@@ -7,9 +7,9 @@ function randomChar(n) {
     let charLength = n;
     let char = "";
 
-    for (var i=0; i<charLength; i++){
+    for (var i = 0; i < charLength; i++) {
         var randomNumber = Math.floor(Math.random() * chars.length);
-        char += chars.substring(randomNumber,randomNumber+1);
+        char += chars.substring(randomNumber, randomNumber + 1);
     }
     return char;
 }
@@ -22,9 +22,9 @@ function randomHex() {
     let chars = "0123456789ABCDEF";
     let hex = "";
 
-    for (var i=0; i<6; i++){
+    for (var i = 0; i < 6; i++) {
         var randomNumber = Math.floor(Math.random() * chars.length);
-        hex += chars.substring(randomNumber,randomNumber+1);
+        hex += chars.substring(randomNumber, randomNumber + 1);
     }
     console.log(`%c #${hex}`, `color: #${hex}`)
 }
@@ -34,12 +34,18 @@ randomHex()
 // Task 3
 
 function randomRGB() {
-    
+
     function randomNumber() {
-       return Math.floor(Math.random() * 256)
+        return Math.floor(Math.random() * 256)
     }
 
-    rgb = `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`
+    rgb = `rgb(${
+        randomNumber()
+    }, ${
+        randomNumber()
+    }, ${
+        randomNumber()
+    })`
 
     console.log(`%c ${rgb}`, `color: ${rgb}`)
 }
@@ -58,9 +64,10 @@ const countries = [
     'Germany',
     'Hungary',
     'Ireland',
+    'Iceland',
     'Japan',
     'Kenya'
-  ]
+]
 
 let COUNTRIES = countries.map(arr => arr.toUpperCase());
 console.log(COUNTRIES);
@@ -76,3 +83,147 @@ console.log(countriesNum)
 
 // Task 6
 
+let arrayOfCountries = [];
+
+for (let i = 0; i < countries.length; i++) {
+    arrayOfCountries.push([
+        countries[i], String(countries[i].slice(0, 3)).toUpperCase(),
+        countries[i].length
+    ])
+}
+
+console.log(arrayOfCountries)
+
+// Task 7
+
+let land = [];
+
+for (let i = 0; i < countries.length; i++) {
+    if (countries[i].includes("land") === true) {
+        land.push(countries[i])
+    }
+}
+
+console.log(land)
+
+// Task 8
+
+let ia = [];
+
+for (let i = 0; i < countries.length; i++) {
+    if (countries[i].includes("ia") === true) {
+        ia.push(countries[i])
+    }
+}
+
+console.log(ia)
+
+// Task 9
+
+let max = 0;
+let longestCountry = "";
+
+for (let i = 0; i < countries.length; i++) {
+    if (countries[i].length > max) {
+        max = countries[i].length;
+        longestCountry = countries[i];
+    }
+}
+
+console.log(longestCountry, max)
+
+// Task 10
+
+let fiveL = [];
+
+for (let i = 0; i < countries.length; i++) {
+    if (countries[i].length === 5) {
+        fiveL.push(countries[i]);
+    }
+}
+
+console.log(fiveL);
+
+// Task 11
+
+
+const webTechs = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB'
+]
+
+const mernStack = ['MongoDB', 'Express', 'React', 'Node']
+
+let webCount = 0;
+let webMax = "";
+
+for (let i = 0; i < webTechs.length; i++) {
+    if (webTechs[i].length > webCount) {
+        webCount = webTechs[i].length;
+        webMax = webTechs[i];
+    }
+}
+
+console.log(webMax, webCount)
+
+// Task 12
+
+let webTechNum = [];
+
+for (let i = 0; i < webTechs.length; i++) {
+    webTechNum.push([webTechs[i], webTechs[i].length])
+}
+
+console.log(webTechNum)
+
+// Task 13
+
+let mern = "";
+
+for (tech in mernStack) {
+    mern += mernStack[tech].slice(0, 1);
+}
+
+console.log(mern);
+
+// Task 14
+
+for (tech in webTechs) {
+    console.log(webTechs[tech])
+}
+
+// Task 15
+
+let fruits = ['banana', 'orange', 'mango', 'lemon'];
+
+let reversedFruits = []; 
+
+for (let i = fruits.length - 1; i >= 0; i--) {
+    reversedFruits.push(fruits[i])
+}
+
+ console.log(reversedFruits);
+
+ // Task 15
+
+ let splitTech = []
+
+ for (tech in webTechs) {
+    splitTech.push(webTechs[tech])
+ }
+
+let fullStack = [];
+
+fullStack.push([splitTech.splice(0, Math.ceil(splitTech.length / 2))], [splitTech])
+
+console.log(fullStack);
+
+
+ for (tech in webTechs) {
+    console.log(webTechs[tech].toUpperCase())
+}
---
+++ 30-days-of-JS/day-6/level3.js
@@ -0,0 +1,314 @@
+const countriesLarge = [
+    'Afghanistan',
+    'Albania',
+    'Algeria',
+    'Andorra',
+    'Angola',
+    'Antigua and Barbuda',
+    'Argentina',
+    'Armenia',
+    'Australia',
+    'Austria',
+    'Azerbaijan',
+    'Bahamas',
+    'Bahrain',
+    'Bangladesh',
+    'Barbados',
+    'Belarus',
+    'Belgium',
+    'Belize',
+    'Benin',
+    'Bhutan',
+    'Bolivia',
+    'Bosnia and Herzegovina',
+    'Botswana',
+    'Brazil',
+    'Brunei',
+    'Bulgaria',
+    'Burkina Faso',
+    'Burundi',
+    'Cambodia',
+    'Cameroon',
+    'Canada',
+    'Cape Verde',
+    'Central African Republic',
+    'Chad',
+    'Chile',
+    'China',
+    'Colombi',
+    'Comoros',
+    'Congo (Brazzaville)',
+    'Congo',
+    'Costa Rica',
+    "Cote d'Ivoire",
+    'Croatia',
+    'Cuba',
+    'Cyprus',
+    'Czech Republic',
+    'Denmark',
+    'Djibouti',
+    'Dominica',
+    'Dominican Republic',
+    'East Timor (Timor Timur)',
+    'Ecuador',
+    'Egypt',
+    'El Salvador',
+    'Equatorial Guinea',
+    'Eritrea',
+    'Estonia',
+    'Ethiopia',
+    'Fiji',
+    'Finland',
+    'France',
+    'Gabon',
+    'Gambia, The',
+    'Georgia',
+    'Germany',
+    'Ghana',
+    'Greece',
+    'Grenada',
+    'Guatemala',
+    'Guinea',
+    'Guinea-Bissau',
+    'Guyana',
+    'Haiti',
+    'Honduras',
+    'Hungary',
+    'Iceland',
+    'India',
+    'Indonesia',
+    'Iran',
+    'Iraq',
+    'Ireland',
+    'Israel',
+    'Italy',
+    'Jamaica',
+    'Japan',
+    'Jordan',
+    'Kazakhstan',
+    'Kenya',
+    'Kiribati',
+    'Korea, North',
+    'Korea, South',
+    'Kuwait',
+    'Kyrgyzstan',
+    'Laos',
+    'Latvia',
+    'Lebanon',
+    'Lesotho',
+    'Liberia',
+    'Libya',
+    'Liechtenstein',
+    'Lithuania',
+    'Luxembourg',
+    'Macedonia',
+    'Madagascar',
+    'Malawi',
+    'Malaysia',
+    'Maldives',
+    'Mali',
+    'Malta',
+    'Marshall Islands',
+    'Mauritania',
+    'Mauritius',
+    'Mexico',
+    'Micronesia',
+    'Moldova',
+    'Monaco',
+    'Mongolia',
+    'Morocco',
+    'Mozambique',
+    'Myanmar',
+    'Namibia',
+    'Nauru',
+    'Nepal',
+    'Netherlands',
+    'New Zealand',
+    'Nicaragua',
+    'Niger',
+    'Nigeria',
+    'Norway',
+    'Oman',
+    'Pakistan',
+    'Palau',
+    'Panama',
+    'Papua New Guinea',
+    'Paraguay',
+    'Peru',
+    'Philippines',
+    'Poland',
+    'Portugal',
+    'Qatar',
+    'Romania',
+    'Russia',
+    'Rwanda',
+    'Saint Kitts and Nevis',
+    'Saint Lucia',
+    'Saint Vincent',
+    'Samoa',
+    'San Marino',
+    'Sao Tome and Principe',
+    'Saudi Arabia',
+    'Senegal',
+    'Serbia and Montenegro',
+    'Seychelles',
+    'Sierra Leone',
+    'Singapore',
+    'Slovakia',
+    'Slovenia',
+    'Solomon Islands',
+    'Somalia',
+    'South Africa',
+    'Spain',
+    'Sri Lanka',
+    'Sudan',
+    'Suriname',
+    'Swaziland',
+    'Sweden',
+    'Switzerland',
+    'Syria',
+    'Taiwan',
+    'Tajikistan',
+    'Tanzania',
+    'Thailand',
+    'Togo',
+    'Tonga',
+    'Trinidad and Tobago',
+    'Tunisia',
+    'Turkey',
+    'Turkmenistan',
+    'Tuvalu',
+    'Uganda',
+    'Ukraine',
+    'United Arab Emirates',
+    'United Kingdom',
+    'United States',
+    'Uruguay',
+    'Uzbekistan',
+    'Vanuatu',
+    'Vatican City',
+    'Venezuela',
+    'Vietnam',
+    'Yemen',
+    'Zambia',
+    'Zimbabwe'
+]
+
+
+const webTechs = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB'
+]
+
+const mernStack = ['MongoDB', 'Express', 'React', 'Node']
+
+
+
+// Task 1
+
+let countriesCopy = [];
+for (country in countriesLarge) {
+    countriesCopy.push(countriesLarge[country])
+}
+
+// Task 2
+
+let sortedCountries = []; 
+
+for (country in countriesLarge) {
+    sortedCountries.push(countriesLarge[country])
+}
+sortedCountries.sort();
+console.log(sortedCountries);
+
+// Task 3
+
+let sortedWebTech = []; 
+
+for (tech in webTechs) {
+    sortedWebTech.push(webTechs[tech])
+}
+
+sortedWebTech.sort()
+
+console.log(sortedWebTech);
+
+let sortedMern = [];
+
+for (mern in mernStack) {
+    sortedMern.push(mernStack[mern])
+}
+
+sortedMern.sort(); 
+
+console.log(sortedMern)
+
+// Task 4
+
+let sortedLand = [];
+
+for (let i = 0; i < countriesLarge.length; i++) {
+    if (countriesLarge[i].includes("land") === true) {
+        sortedLand.push(countriesLarge[i])
+    }
+}
+
+sortedLand.sort();
+console.log(sortedLand);
+
+// Task 5
+
+let max2 = 0;
+let longestCountry2 = "";
+
+for (let i = 0; i < countriesLarge.length; i++) {
+    if (countriesLarge[i].length > max2) {
+        max2 = countriesLarge[i].length;
+        longestCountry2 = countriesLarge[i];
+    }
+}
+
+console.log(longestCountry2, max2)
+
+// Task 6
+
+let fourChars = [];
+
+for (let i = 0; i < countriesLarge.length; i++) {
+    if (countriesLarge[i].length  === 4) {
+        fourChars.push(countriesLarge[i])
+    }
+}
+
+console.log(fourChars);
+
+// Task 7
+
+let spaces = [];
+
+for (let i = 0; i < countriesLarge.length; i++) {
+    if (countriesLarge[i].includes(" ") === true) {
+        spaces.push(countriesLarge[i])
+    }
+}
+
+spaces.sort();
+console.log(spaces);
+
+
+// Task 8 
+
+let reversedCapsCountries = []; 
+
+for (country in countriesLarge) {
+    reversedCapsCountries.push(countriesLarge[country].charAt(0).toUpperCase() + countriesLarge[country].substring(1))
+}
+
+reversedCapsCountries.sort().reverse()
+
+console.log(reversedCapsCountries)
