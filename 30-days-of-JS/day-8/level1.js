4efb8de5bce7159202ece41cfdf2ace8a3c4a6d5
Pakesy <conorpakes@gmail.com>
2023-01-30 18:18:15 +1100

    Day 8 Level 1

--- 30-days-of-JS/day-8/level1.js
+++ 30-days-of-JS/day-8/level1.js
@@ -1,1 +1,34 @@
 /* Day 8 Level 1 Challenges */ 
+
+// Task 1
+
+const dog = {};
+
+// Task 2 
+
+console.log(dog);
+
+// Task 3
+
+dog.name = "Chooky";
+dog.legs = 4; 
+dog.color = "black";
+dog.age = 15;
+dog.bark = function() {
+    return "Woof Woof"
+}
+
+console.log(dog)
+
+// Task 4
+
+console.log(dog.name, dog.legs, dog.color, dog.age, dog.bark());
+
+// Task 5
+
+dog.breed = "Mini Foxy / Jack Russell"; 
+dog.getDogInfo = function() {
+    return `${dog.name}, ${dog.legs}, ${dog.color}, ${dog.age}, ${dog.breed}, ${dog.bark()}`
+}
+console.log(dog.getDogInfo())
+
